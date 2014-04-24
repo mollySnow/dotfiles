9e02282a774c5dd18517fe65349524adc34a2f55
Magnus Björk <magnusbjork@Magnuss-MacBook-Pro.local>
2014-04-24 21:14:26 +0200

    dotfiles on git! :D

---
+++ mInstall.sh
@@ -0,0 +1,27 @@
+#### Variables
+
+dir=~/dotfiles
+olddir=~/dotfiles_old
+files="vimrc vim"
+
+#### Backup old dotfiles
+
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+#### 
+
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+#### 
+
+for file in $files; do
+    echo "Moving any existing dotfiles from ~to $olddir"
+    mv ~/.$file ~/dotfiles_old/ 
+    echo "Creating symlink to $file in home directory"
+    ln -s $dir/$file ~/.$file
+done
+
